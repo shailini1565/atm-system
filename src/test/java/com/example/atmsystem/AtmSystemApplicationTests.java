83b7aad0e16d6ed8ba031906f0744256068c4e56
Shalini Bhataniya <shalinibhataniya1097@gmail.com>
2022-07-27 12:58:25 +0530

    initial commit

---
+++ src/test/java/com/example/atmsystem/AtmSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atmsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
